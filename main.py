e16bf01e771ef364c5c8e27bcb267a6ea7fa7a73
Jed Duffey <jedduffey@gmail.com>
2020-12-24 20:06:07 -0500

    extract message values

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
-message =\
-"{\"cameraId\": \"2aa1fd44-4311-4e4c-a817-ea000449e23b\", \
-\"timestamp\": \"1608857171\", \
-\"signal\": \"[0,1,2,3]\"}"
+camera_id = "2aa1fd44-4311-4e4c-a817-ea000449e23b"
+timestamp = "1608857171"
+signal = "[0,1,2,3]"
+
+message = f"{{\"cameraId\": \"{camera_id}\", \"timestamp\": \"{timestamp}\", \"signal\": \"{signal}\"}}"
 
 
-def send(message):
+def send(msg):
     print("Sending message...")
-    print(f'Message: {message}')
+    print(f'Message: {msg}')
 
 
 if __name__ == '__main__':
