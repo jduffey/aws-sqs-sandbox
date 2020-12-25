dae3453b6a451ca94208abc972a77ae2d14a55e1
Jed Duffey <jedduffey@gmail.com>
2020-12-24 20:33:10 -0500

    loop to print multiple messages

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ def construct_message():
     timestamp = int(time.time())
     signal = []
 
-    for i in range(4):
+    for _ in range(4):
         signal.append(random.randint(0, 3))
 
     return json.dumps({'cameraId': camera_id, 'timestamp': timestamp, 'signal': signal}, separators=(',', ':'))
 
 
 def send(msg):
-    print("Sending message...")
     print(f'Message: {msg}')
 
 
 if __name__ == '__main__':
-    message = construct_message()
-    send(message)
+    print("Sending message(s)...")
+    for _ in range(10):
+        message = construct_message()
+        send(message)
