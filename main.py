90545c18b96d364fbbc4f84aa86367f5bc757a1e
Jed Duffey <jedduffey@gmail.com>
2020-12-24 20:16:59 -0500

    construct json using json module

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import json
+
 
 camera_id = "2aa1fd44-4311-4e4c-a817-ea000449e23b"
 timestamp = "1608857171"
 signal = "[0,1,2,3]"
 
-message = f"{{\"cameraId\": \"{camera_id}\", \"timestamp\": \"{timestamp}\", \"signal\": \"{signal}\"}}"
+message = json.dumps({'cameraId': camera_id, 'timestamp': timestamp, 'signal': signal}, separators=(',', ':'))
 
 
 def send(msg):
