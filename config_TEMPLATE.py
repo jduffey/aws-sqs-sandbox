945f1ca7af36020e5c8bf82da524d06f33cd58c1
Jed Duffey <jedduffey@gmail.com>
2020-12-24 22:15:41 -0500

    remove info links from config template

--- config_TEMPLATE.py
+++ config_TEMPLATE.py
@@ -3,12 +3,3 @@ region_name = "<e.g. 'us-east-1'>"
 aws_access_key_id = "<AWS ACCESS KEY ID>"
 aws_secret_access_key = "<AWS SECRET ACCESS KEY>"
 queue_name = "<NAME OF THE QUEUE>"
-
-'''
-For info on getting your AWS Access Key Id and AWS Secret Access Key, see
-https://docs.aws.amazon.com/general/latest/gr/aws-sec-cred-types.html#access-keys-and-secret-access-keys
-
-More info on working programmatically with AWS SQS:
-https://www.youtube.com/watch?v=5wxC6O1X440
-https://github.com/soumilshah1995/AWSSQS-Python-Tutorial
-'''
