ba5e7ac7dcdcfa8916e7c825231183e6a1b73843
Jed Duffey <jedduffey@gmail.com>
2020-12-24 21:43:06 -0500

    refactor

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 import time
-import boto3
-import config
 
 from utils import construct_message, send_message
 
 
-client = boto3.resource(config.resource,
-                        region_name=config.region_name,
-                        aws_access_key_id=config.aws_access_key_id,
-                        aws_secret_access_key=config.aws_secret_access_key)
-
-queue = client.get_queue_by_name(QueueName=config.queue_name)
-
-
 if __name__ == '__main__':
-    response = queue.send_message(MessageBody='Hello world!')
-
-    print(response)
-
-    # print("Sending message(s)...")
-    # for _ in range(10):
-    #     message = construct_message()
-    #     send_message(message)
-    #     time.sleep(0.25)
+    print("Sending message(s)...")
+    for _ in range(5):
+        message = construct_message()
+        response = send_message(message)
+        print(message)
+        print(response)
+        time.sleep(1)
--- utils.py
+++ utils.py
@@ -3,6 +3,18 @@ import uuid
 import time
 import random
 
+import boto3
+
+import config
+
+
+client = boto3.resource(config.resource,
+                        region_name=config.region_name,
+                        aws_access_key_id=config.aws_access_key_id,
+                        aws_secret_access_key=config.aws_secret_access_key)
+
+queue = client.get_queue_by_name(QueueName=config.queue_name)
+
 
 def construct_message():
     camera_id = str(uuid.uuid4())
@@ -19,4 +31,4 @@ def construct_message():
 
 
 def send_message(msg):
-    print(f'Message: {msg}')
+    return queue.send_message(MessageBody=msg)
